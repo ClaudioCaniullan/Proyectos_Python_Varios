40076f58ab9aa042e7c1edc907772c0c326be18e
Claudio Caniullan <30704079+ClaudioCaniullan@users.noreply.github.com>
2022-02-22 23:26:25 -0300

    Lector de archivos pdf
    Transforma en voz el texto que viene incluido en un archivo pdf

---
+++ lector_pdf.py
@@ -0,0 +1,22 @@
+
+## Lector de textos en pdf ##
+
+# importamos librerias
+from mimetypes import init
+import PyPDF2
+import pyttsx3
+
+# Cargamos el archivo pdf
+libro=open('archivo.pdf', 'rb')
+lector=PyPDF2.PdfFileReader(libro)
+
+# creamos una instancia de voz, cargamos la pagina del texto en pdf y extraemos su texto
+# numero_pagina corresponde a la página del libro que queremos tranformar a voz 
+voz=pyttsx3.init()
+pagina=lector.getPage(#numero_pagina)
+texto=pagina.extractText()
+
+# cargams el texto para que voz.say lo reproduzca en voz 
+voz.setProperty('rate', 140)
+voz.say(texto)
+voz.runAndWait()
